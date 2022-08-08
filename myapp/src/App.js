bc166ac1502555f191c1194653f7351796f66e69
vhoreho <vpubuntu@gmail.com>
2022-08-08 22:10:04 +0300

    complete todo task

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Input from './components/Input';
 import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
-      <Input />
       <Todos />
     </div>
   );
--- myapp/src/components/TodoItem.js
+++ myapp/src/components/TodoItem.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export default function TodoItem() {
-  return (
-    <li>1</li>
-  )
+export default function TodoItem(props) {
+    const { id, copmpleted, title, deleteTodo } = props;
+    return (
+        <li style={{
+            background: '#eee',
+            display: 'block',
+            margin: '10px 0',
+            padding: '10px',
+            display: 'flex',
+            justifyContent: 'space-between'
+        }}>
+            {`${id}. ${title}`}
+            <button onClick={() => deleteTodo(id)}>Delete</button>
+        </li>
+    )
 }
